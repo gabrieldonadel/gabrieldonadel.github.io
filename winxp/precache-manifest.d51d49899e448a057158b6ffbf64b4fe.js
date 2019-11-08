decb1f5312ff5138c7c167a8563123b877fa190a
Gabriel Donadel <gabidonadel1@gmail.com>
2019-11-08 00:41:44 -0300

    WinXP v0.0.1

---
+++ winxp/precache-manifest.d51d49899e448a057158b6ffbf64b4fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d989ffcb7ba233eaa0559fd326ad1bd2",
+    "url": "/winxp/index.html"
+  },
+  {
+    "revision": "5460e71fd169d237ec8c",
+    "url": "/winxp/static/css/main.c65623f0.chunk.css"
+  },
+  {
+    "revision": "d444e059317480796993",
+    "url": "/winxp/static/js/2.316b48d0.chunk.js"
+  },
+  {
+    "revision": "5460e71fd169d237ec8c",
+    "url": "/winxp/static/js/main.d0ba59af.chunk.js"
+  },
+  {
+    "revision": "d3c42a61137b4819bff9",
+    "url": "/winxp/static/js/runtime~main.361da0d9.js"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "/winxp/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "/winxp/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "/winxp/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "/winxp/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "/winxp/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "/winxp/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "/winxp/static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "/winxp/static/media/view-info.415b027d.ico"
+  }
+]);
