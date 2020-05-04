b7058baeb2073650320d444c754b349c6fd9d535
Gabriel Donadel <gabidonadel1@gmail.com>
2020-05-04 00:42:14 -0300

    test
    test

---
+++ LandingPageLinx/precache-manifest.f56d7abf548641b951c35a8941014fc3.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93be7d2df0f63662e4efbb4b18c8fd01",
+    "url": "index.html"
+  },
+  {
+    "revision": "78632adca3356f7747bd",
+    "url": "main.js"
+  }
+]);
